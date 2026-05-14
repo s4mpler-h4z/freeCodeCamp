a92eb67d137456d0de8d2401f4884cdd06cca6b6
s4mpler-h4z <bry4nsimoes@gmail.com>
2026-05-14 15:16:53 -0300

    Add files via upload
    Exibe nome completo, idade, bairro e zona de convivencia.

---
+++ # joca_central.py
@@ -0,0 +1,11 @@
+str_name = 'Joca'
+str_name_second_part = "Propicio"
+
+str_bairro = "Santa Cecilia"
+str_regiao = 'Zona Central' 
+age = 16
+
+print( str_name + ' ' + str_name_second_part + ' ' + str(age))
+print(str_bairro + '-' + str_regiao)
+
+# exibe o nome completo do Joca, a idade e o bairro onde ele mora.
